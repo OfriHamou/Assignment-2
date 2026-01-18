05176a40f0f1b1f6254773e7ccdcd86c88337d92
ofrihamou <ofrihemo1@gmail.com>
2026-01-18 15:46:18 +0200

    removed .env for simplicitty

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
-import dotenv from 'dotenv';
 import express, { Express } from 'express';
 import mongoose from 'mongoose';
 import postRoutes from './routes/postRoute';
 import commentRoutes from './routes/commentRoute';
 import userRoutes from './routes/userRoute';
 
-// Load environment variables from .env file
-dotenv.config();
+const PORT: number = 3000;
 
-const PORT: number = parseInt(process.env.PORT || '3000');
 // Create Express app
 const app: Express = express();
 
@@ -16,7 +13,7 @@ const app: Express = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/assignment2');
+mongoose.connect('mongodb://localhost:27017/assignment2');
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
